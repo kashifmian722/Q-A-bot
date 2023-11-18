e380bf0af3b49acf56ceadec9cdc9f9c4181b601
kashifmian722 <64021544+kashifmian722@users.noreply.github.com>
2023-11-18 11:29:38 +0500

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -5,50 +5,36 @@ import os
 
 load_dotenv()
 
-co = cohere.Client(os.getenv('COHERE_API_KEY'))
+co = cohere.Client
 
-def generate_ans(qstn):
+@st.cache(persist=True)
+def co_chat(message, model, temperature, chat_history, prompt_truncation, stream, citation_quality, connectors, documents):
+    co = cohere.Client(os.getenv('COHERE_API_KEY'))
+    response = co.chat(
+        model=model,
+        message=message,
+        temperature=temperature,
+        chat_history=chat_history,
+        prompt_truncation=prompt_truncation,
+        stream=stream,
+        citation_quality=citation_quality,
+        connectors=connectors,
+        documents=documents,
+    )
+    return response
 
-  response = co.generate(model=cohere_model_id,prompt=qstn)
+message = st.text_input("Enter your message:")
 
-  bot_answer = response.generations[0].text
-  bot_answer = bot_answer.replace("\n\n--","").replace("\n--","").strip()
+response = co_chat(
+    message=message,
+    model="command",
+    temperature=0.3,
+    chat_history=[],
+    prompt_truncation="auto",
+    stream=True,
+    citation_quality="accurate",
+    connectors=[{"id": "web-search", "options": {"site": "https://myvitaminstore.pk"}}],
+    documents=[],
+)
 
-  return bot_answer
-
-
-# The front end code starts here
-
-st.title("Question & answer bot with Cohere")
-
-form = st.form(key="user_settings")
-with form:
-  cohere_api_key = st.text_input('Cohere API Key:', type='password')
-  cohere_model_id = st.text_input('Cohere Model Id:')
-
-  if not cohere_api_key and not cohere_model_id:
-    st.info("Please add your Cohere API key and Custom model key or use 'medium/xlarge' to continue.")
-    update_api_keys = form.form_submit_button("Update keys")
-    st.stop()
-
-  co = cohere.Client(cohere_api_key)
-
-  st.write("Enter your qstn [Example: Who is the PM of UK] ")
-
-  qstn_input = st.text_input("Question", key = "qstn_input")
-    
-  generate_button = form.form_submit_button("Answer Question")
-
-  if generate_button:
-    if qstn_input == "":
-      st.error("Question field cannot be blank")
-    else:
-      my_bar = st.progress(0.05)
-      st.subheader("Answer from bot:")
-
-      for i in range(1):
-          st.markdown("""---""")
-          ans = generate_ans(qstn_input)
-          st.markdown("##### " + ans)
-          st.write(ans)
-          my_bar.progress((i+1)/1)
+st.markdown(response)
